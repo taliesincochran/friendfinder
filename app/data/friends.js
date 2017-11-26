46dd2afe733bb51dce75249ae4fa6669c7998386
Taliesin <tdcochran2@gmail.com>
2017-11-26 11:22:00 -0500

    Added variation to friends array

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,32 +4,32 @@ module.exports = {
 		{
 			name: "Charlie Brown",
 			photo: "https://upload.wikimedia.org/wikipedia/en/2/22/Charlie_Brown.png",
-			scores: [1,1,1,1,1,1,1,1,1,1]
+			scores: [1,3,5,1,1,1,1,1,1,1]
 		},
 		{
 			name: "Lucy Van Pelt",
 			photo: "https://upload.wikimedia.org/wikipedia/en/e/e9/Lucy_van_Pelt.png",
-			scores: [5,4,2,1,5,2,5,5,3,1]
+			scores: [5,3,2,5,5,2,5,3,1,1]
 		},
 		{
 			name: "Linus Van Pelt",
 			photo: "https://upload.wikimedia.org/wikipedia/en/5/5e/Linus_van_Pelt.gif",
-			scores: [2,3,5,4,1,2,3,6,2,5]
+			scores: [2,3,3,1,1,2,3,3,3,3]
 		},
 		{
 			name: "Pig-Pen",
 			photo: "https://upload.wikimedia.org/wikipedia/en/e/e1/PigPen_%28Peanuts%29.png",
-			scores: [1,2,4,4,2,3,2,4,5,3]
+			scores: [1,5,5,1,1,3,1,1,4,5]
 		},
 		{
 			name: "Peppermint Patty",
 			photo: "https://upload.wikimedia.org/wikipedia/en/a/a0/Peppermint_Patty.png",
-			scores: [3,2,5,4,5,5,1,2,3,2]
+			scores: [3,2,3,3,4,2,3,3,2,3]
 		},
 		{
 			name: "Marice Johnson",
 			photo: "https://upload.wikimedia.org/wikipedia/en/2/24/Marcie_from_Peanuts.png",
-			scores: [3,2,5,4,2,5,1,2,3,2]
+			scores: [1,2,3,4,2,4,1,2,3,5]
 		},
 	]
 }
